28f2c7a8d556aaad4dd359c008608086d1cd7bfd
Harold Abreu Ramírez <harverbo@hotmail.com>
2021-05-31 21:39:11 -0400

    Fixed commands routes

--- src/BreadGeneratorServiceProvider.php
+++ src/BreadGeneratorServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Harverbo\BreadGenerator;
 
 use Illuminate\Support\ServiceProvider;
+use Harverbo\BreadGenerator\Console\Commands\BreadRowsGenerator;
+use Harverbo\BreadGenerator\Console\Commands\BreadStubGenerator;
 
 class BreadGeneratorServiceProvider extends ServiceProvider
 {
